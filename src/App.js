a1e997d68ad27eb52469dbb8870fd60163a71529
sklee <sklee385@gmail.com>
2018-08-05 23:19:45 +0900

    jsx 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    const text = "당신은 어썸한가요?";
+    const condition = true ;
+    const style = {
+      backgroundColor : 'gray',
+      border : "1px solid black",
+      height : Math.round(Math.random() * 300) + 50,
+      width : Math.round(Math.random() * 300) + 50 ,
+      WebkitTransition : 'all',
+      MozTransition : "all",
+      msTransition : "all"
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="my-div">
+        <h1>리액트 안녕</h1>
+        <h2>{text}</h2>
+        { condition ? "참" : "거짓"}<br/>
+        { condition && "보여주세요" }
+        {
+          // 주석 1 
+          /* 주석 2 */
+        }
+        <div
+        // 여기 주석은 동작 합니다. 
+        // 하지만 /> 는 따로 줄 바뀜을 표시해야 합니다. 
+        // 왜냐면 // 주석은 한줄 통채 주석이기 떄문입니다. 
+        /* */
+        />
+        <div style={style}></div>
       </div>
     );
   }
