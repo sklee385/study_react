fea2db7ae3027e10fcad0f1c7ac24e16cbefe94f
sklee <sklee385@gmail.com>
2018-08-20 22:52:42 +0900

    css module study project setting

---
+++ styling-react/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import styles from './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div >
+                {/* 
+                    <div class={[styles.box, styles.blue].join(' ')}>
+                    </div>
+                */}
+            </div>
+        );
+    }
+}
+
+export default App;
