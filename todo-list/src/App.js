2cda156d9a9323dd0c658be8adc2a678b300bcec
sklee <sklee385@gmail.com>
2018-08-31 08:36:06 +0900

    todolist update

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,21 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList'
+import Palette from './components/Palette'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    id = 3;
+    
+    state = {
+        input: '',
+        todos: [
+            { id: 0, text: '리액트 소개', checked: false },
+            { id: 1, text: 'JSX사용하기', checked: true },
+            { id: 2, text: '라이프 사이클 이해하기', checked: false }
+        ],
+        color : ''
+    }
+
+    handleToggle = (id) => {
+        const { todos } = this.state;
+
+        // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+        const index = todos.findIndex(todo => todo.id === id);
+        const selected = todos[index]; // 선택한 객체
+
+        const nextTodos = [...todos]; // 배열을 복사
+
+        // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+        nextTodos[index] = {
+            ...selected,
+            checked: !selected.checked
+        };
+
+        this.setState({
+            todos: nextTodos
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value // input 의 다음 바뀔 값
+        });
+    }
+
+    handleCreate = () => {
+        const { input, todos } = this.state;
+        this.setState({
+            input: '', // 인풋 비우고
+            // concat 을 사용하여 배열에 추가
+            todos: todos.concat({
+                id: this.id++,
+                text: input,
+                checked: false
+            })
+        });
+
+    }
+
+    handleKeyPress = (e) => {
+        // 눌려진 키가 Enter 면 handleCreate 호출
+        if (e.key === 'Enter') {
+            this.handleCreate();
+        }
+    }
+
+    handleRemove = (id) => {
+        const { todos } = this.state;
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        });
+    }
+
+    handlePaletteClick = (color)=>{
+        this.setState({
+            color : color
+        })
+    }
+
+    render() {
+        const { input, todos, color } = this.state;
+        const {
+            handleChange,
+            handleCreate,
+            handleKeyPress,
+            handleToggle,
+            handleRemove,
+            handlePaletteClick
+        } = this;
+        return (
+            <div >
+                
+                <TodoListTemplate 
+                    form={
+                        <Form
+                            value={input}
+                            onKeyPress={handleKeyPress}
+                            onChange={handleChange}
+                            onCreate={handleCreate}
+                        />
+                    }
+                    palette={
+                        <Palette colors={['#343a40', '#f03e3e', '#12b886', '#228ae6']}  onclick={handlePaletteClick}/>
+                    }
+                    color={color}
+                >
+                    <TodoItemList todos={todos} onToggle={handleToggle}  onRemove={handleRemove} />
+                </TodoListTemplate>
+            </div>
+        );
+    }
 }
 
 export default App;
